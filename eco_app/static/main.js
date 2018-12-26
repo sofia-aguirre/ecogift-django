c5c1bf5b03d595a90e7cdcd9250bd00f725feea8
sofia-aguirre <sofiaxaguirre@gmail.com>
2018-12-25 23:23:42 -0800

    added overlay to carousel, fixed landing links

--- eco_app/static/main.js
+++ eco_app/static/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 
+var carHi= $('#carousel').css('height');
+$('#carousel-overlay').css('height', carHi);
+
 $('.prod-img').mouseover(function(){
   var imgSrc= $(this).attr('src');
   $('#prod-zoom').attr('src', imgSrc);
