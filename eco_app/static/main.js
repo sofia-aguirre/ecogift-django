d38e96b62a8fc7e66eac4ca2637cf9a79261aff0
sofia-aguirre <sofiaxaguirre@gmail.com>
2018-12-25 21:43:53 -0800

    finished product detail page

--- eco_app/static/main.js
+++ eco_app/static/main.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
 
-//   var userFeed = new Instafeed({
-//     get: 'user',
-//     userId: '3710433476',
-//     limit: 6,
-//     sortBy: 'most-recent',
-//     template: '<a href="{{image}}"><img src="{{image}}"/></a>',
-//     accessToken: '282691147.1677ed0.ccc82718b47242939e474d2af5b4246d'
-// });
-// userFeed.run();
+$('.prod-img').mouseover(function(){
+  var imgSrc= $(this).attr('src');
+  $('#prod-zoom').attr('src', imgSrc);
+});
 
+$('.prod-img').mouseout(function(){
+  var nextSrc= $('.prod-img')[0].attr('src');
+  $('#prod-zoom').attr('src', nextSrc);
+});
 
 });
